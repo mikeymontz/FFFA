da6f87d2e5b439ff05cda9a37ae38801ebc42a3c
James Armetta <jamesarmetta@users.noreply.github.com>
2018-05-01 18:23:51 -0400

    Data for survey
    Broken
    starts when survey starts

--- finished product/index.js
+++ finished product/index.js
@@ -1,32 +1,25 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-
     document.getElementById("user_div").style.display = "block";
     document.getElementById("login_div").style.display = "none";
 
     var user = firebase.auth().currentUser;
 
     if(user != null){
-
       var email_id = user.email;
       var email_verified = user.emailverified;
       document.getElementById("user_para").innerHTML = "Welcome: " + email_id +
                                                         "<br/>Verified: " + email_verified;
-
     }
-
   } else {
     // No user is signed in.
-
     document.getElementById("user_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
-
   }
 });
 
 function login(){
-
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
@@ -36,13 +29,10 @@ function login(){
     var errorMessage = error.message;
 
     window.alert("Error : " + errorMessage);
-
     // ...
   });
-
 }
 function create_account(){
-
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
@@ -52,13 +42,10 @@ function create_account(){
     var errorMessage = error.message;
 
     window.alert("Error : " + errorMessage);
-
     // ...
   });
-
 }
 function send_verification(){
-
   var user = firebase.auth().currentUser;
 
   user.sendEmailVerification().then(function(){
@@ -72,3 +59,13 @@ function send_verification(){
 function logout(){
   firebase.auth().signOut();
 }
+
+//creats DB for the users scores
+var database = firebase.database();
+var ref = database.ref('scores');
+
+var data = {
+  name: "userEmail",
+  score: 5
+}
+ref.push(data);
